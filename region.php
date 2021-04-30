6f1e18d1ba685ffcb7ef2acab67c22cc98e52802
mame-fatou <ndeyelebayame@gmail.com>
2021-04-30 19:59:36 +0200

    pour la carte

---
+++ region.php
@@ -0,0 +1,57 @@
+<?php include 'bd.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+
+    <title>Page Villes </title>
+	 <?php
+	include 'header.php';
+	?>
+	
+	
+   <h1 class="titre">Régions </h1>  
+ 
+ 
+ <p> Choisissez la région qui vous intéresse afin de voir toutes les villes disponibles et obtenir des informations !</p></br>   
+
+	
+	<script src="cmap/france-map.js"></script>
+
+	<script>francefree();</script>  
+	
+
+<style>
+a:hover {background-color:red;
+text-align:center;}
+
+
+
+
+p {
+    font-size: 20px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-weight: normal;
+    text-align: center;
+    color:black;
+
+body {
+    
+    text-align: left;
+  
+
+
+</style>
+	
+
+</head>
+
+<body>
+
+  
+  </body>
+
+</html>
