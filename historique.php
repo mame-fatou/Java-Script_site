1c5e4d5e0c4f19429a10f670a56b4338f9cd7bfd
mame-fatou <ndeyelebayame@gmail.com>
2021-04-30 20:16:15 +0200

    historique de tous les graphes de tous les gare

---
+++ historique.php
@@ -0,0 +1,88 @@
+<?php include 'bd.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+
+    <title>HISTORIQUE graphes</title>
+		
+	 <?php
+	include 'header.php';
+	?>
+	
+
+</head>
+<body>
+  
+  
+  
+  
+  
+  
+  
+  
+ <a href="repertoire\Bordeaux.php"> Bordeaux</a></br>
+ 
+  
+
+   <a href="repertoire\Bordeaux.php"> Bordeaux</a></br>
+
+   
+    <a href="repertoire\charle_Degaulle.php"> Paris Charle De gaulle</a></br>
+      <a href="repertoire\Paris_Lyon.php"> Paris_Lyon</a></br>
+   <a href="repertoire\Lille_Europe.php"> Lille Europe</a></br>
+     <a href="repertoire\Paris_st-lazard.php"> Paris Saint lazard</a></br>
+	  
+	  <a href="repertoire\Reims.php"> Reims</a></br>
+   
+   <a href="repertoire\Paris_Nord.php"> Paris Nord</a></br>
+   
+   <a href="repertoire\Paris_Montparnasse.php"> Paris Montparnasse</a></br>
+   
+   <a href="repertoire\Paris_Est.php"> Paris Est</a></br>
+   
+   <a href="repertoire\Paris_Bercy.php"> Paris_Bercy</a></br>
+      <a href="repertoire\Paris_Austerlitz.php"> Paris Austerlitz</a></br>
+   
+ 
+   <a href="repertoire\Massy.php"> Massy</a></br>
+   
+   <a href="repertoire\Valence.php"> Valence</a></br>
+   
+   <a href="repertoire\Toulouse_Matabiau.php"> Toulouse Matabiau</a></br>
+   
+   <a href="repertoire\Toulon.php"> Toulon</a></br>
+      <a href="repertoire\Bordeaux.php"> Bordeaux</a></br>
+
+   
+   <a href="repertoire\Nice.php"> Nice</a></br>
+   
+   <a href="repertoire\Montpellier.php"> Montpellier</a></br>
+   
+   <a href="repertoire\Marseille.php"> Marseille</a></br>
+   
+   <a href="repertoire\Lyon_Exupery.php"> Lyon Saint Exupery</a></br>
+   
+   <a href="repertoire\Lille_Europe.php"> Lille Europe</a></br>
+   
+   <a href="repertoire\Strasbourg.php"> Strasbourg</a></br>
+   
+   <a href="repertoire\Marne_la _vallée.php"> Marne_la _vallée</a></br>
+   
+   <a href="repertoire\graph_Rennes.php"> Rennes</a></br>
+   
+
+   
+
+   <a href="repertoire\Lille_Flandre.php"> Lille Flandre</a></br>
+   
+   <a href="repertoire\Lille_Europe.php"> Lille Europe</a></br>
+   
+   <a href="repertoire\Nantes.php"> Nantes</a></br>
+
+    <a href="repertoire\graph_Rennes.php"> Rennes </a></br>
+
+</body>
+</html>
