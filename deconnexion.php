94f74ab316e84ad4ff4353e10dd66bb74ff4174e
mame-fatou <ndeyelebayame@gmail.com>
2021-04-30 20:09:32 +0200

    la page deccoxion

---
+++ deconnexion.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+session_destroy();
+
+?>
+<!DOCTYPE HTML>
+
+
+<HTML>
+<HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<TITLE>page de deconnexion</TITLE>
+
+<?php
+			include('header.php');?>
+</HEAD>
+<body>
+<h1> VOY'AGES </h1>
+
+<img width=400px src ="images/gare.jpg" alt="photo"> 
+
+ <H1>A bientot !</H1>
+ 
+
+
+ 
+</body>
+</html>
