a372c5fc0bcf90973092b7af62d62830c602f9d8
ndeyelebayame@gmail.com <ndeyelebayame@gmail.com>
2021-04-30 19:26:37 +0200

    ajout de la page haeder qui contient les entetes

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang>
+
+
+<div class="navbar">
+	<a href="accueilX.php">Home</a>
+	<a href="ville.php">Ville</a>
+	<a href="region.php">Carte</a>
+	<a href="historique.php">statistique</a>
+	<a href="avis.php">Avis</a>
+	
+	
+<?php
+if(!isset($_SESSION['utilisateur'])) {  ?>
+<a href="inscription.php">S'inscrire </a>
+<a  href="connexion.php">Se connecter</a> 
+<?php
+} else {
+    echo "Vous êtes connecté(e)s en tant que ";
+    echo $_SESSION["utilisateur"]['prenom'].' ';
+    echo $_SESSION["utilisateur"]['nom']; 
+    
+    echo'<a href="deconnexion.php">Se déconnecter</a> ';
+ }
+?>
+	
+	
+</div>
+</html>
